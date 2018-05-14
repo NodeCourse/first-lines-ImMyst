082fb4a4de408ac8bce8f357011fc3358b17b5db
ImMyst <titouan.galvani@laposte.net>
2018-05-14 10:26:14 +0200

    Date writing in a file every second

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
+// Function adding current timestamp to date.txt file
+
 function getDateEverySecond() {
   fs.appendFile("date.txt", Date.now() + "\r\n", (error) => {
     if(error) return console.log("Erreur lors de l'écriture du timestamp : " + error);
   });
 
-  console.log("Currently writing", Date.now());
+  console.log("Currently writing", Date.now() + " in date.txt file");
 }
 
 setInterval(getDateEverySecond, 1000);
